3e41991db7525558a9a451de91cd3d4df8fe393e
marti149 <marti149@new-mint>
2014-11-13 16:05:00 -0600

    First commit

---
+++ Primes.R
@@ -0,0 +1,14 @@
+primes = function(n) {
+      if (n >= 2) {
+          s = seq(2,n)
+          p = c()
+          for (i in seq(2,n)) {
+            if(any(s==i)){
+             p = c(p,i)
+             s = c(s[(s%%i) != 0],i )
+          }} 
+      return (p) }
+     else {
+          stop("Input at least 2")
+      }
+    }
